d59a8e977868fcfce2c8c07086e9d1d45eae2f4c
PlankE68 <yarden.karsh@gmail.com>
2022-06-03 13:12:15 +0300

    code clean up + correct resizing for both width and height

--- app.py
+++ app.py
@@ -8,39 +8,23 @@ import constants
 
 
 class ChessValues(Enum):
+    BLACK = -1
+    EMPTY = 0
     WHITE = 1
-    BLACK = 2
-    MOVE = 3
-    CAPTURE = 4
+    HIGHLIGHT = 2
+    HOVER = 3
+    MOVE = 4
+    CAPTURE = 5
+    CHECK = 6
 
 
 def assign_chess_board_values():
-    return [
-        [[ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE],
-         [ChessValues.BLACK],
-         [ChessValues.WHITE], [ChessValues.BLACK]],
-        [[ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK],
-         [ChessValues.WHITE],
-         [ChessValues.BLACK], [ChessValues.WHITE]],
-        [[ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE],
-         [ChessValues.BLACK],
-         [ChessValues.WHITE], [ChessValues.BLACK]],
-        [[ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK],
-         [ChessValues.WHITE],
-         [ChessValues.BLACK], [ChessValues.WHITE]],
-        [[ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE],
-         [ChessValues.BLACK],
-         [ChessValues.WHITE], [ChessValues.BLACK]],
-        [[ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK],
-         [ChessValues.WHITE],
-         [ChessValues.BLACK], [ChessValues.WHITE]],
-        [[ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE],
-         [ChessValues.BLACK],
-         [ChessValues.WHITE], [ChessValues.BLACK]],
-        [[ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK], [ChessValues.WHITE], [ChessValues.BLACK],
-         [ChessValues.WHITE],
-         [ChessValues.BLACK], [ChessValues.WHITE]],
-    ]
+    board = []
+    for i in range(8):
+        board.append([])
+        for j in range(8):
+            board[i].append(ChessValues.BLACK if (i+j) % 2 else ChessValues.WHITE)
+    return board
 
 
 class ChessPiece:
@@ -52,36 +36,33 @@ class ChessPiece:
 
 class Pawn(ChessPiece):
     def __init__(self, color, x, y):
-        ChessPiece.__init__(color, x, y)
+        ChessPiece.__init__(self, color, x, y)
 
     def available_moves(self, board):
         moves = []
 
-        move_up = 1 if self.color == ChessValues.WHITE else 1
+        move_up = self.color.value
 
-        if self.color == ChessValues.WHITE:
-            if board[self.y + 1][self.x] == 0 or board[self.y + 1][self.x] == 0:
-                moves.append((0, 1))
-        else:
-            if self.color == ChessValues.BLACK:
-                if board[self.y - 1][self.x] == 0 or board[self.y - 1][self.x] == 0:
-                    moves.append((0, -1))
+        if board[self.y + move_up][self.x] == 0:
+            moves.append((0, move_up))
 
-        if self.y == 1:
-            if board[self.y + 2 * move_up][self.x] == 0 or board[self.y + 2 * move_up][self.x] == 0:
+        if (self.y == 1 and move_up == 1) or (self.y == 6 and move_up == -1):
+            if board[self.y + 2 * move_up][self.x] == 0 and board[self.y + move_up][self.x] == 0:
                 moves.append((0, 2 * move_up))
+
         if self.x > 0:
-            if board[self.y + 1 * move_up][self.x - 1] != 0 and board[self.y + 1 * move_up][self.x - 1].color == ChessValues.BLACK:
+            if board[self.y + 1 * move_up][self.x - 1].color.value == -1 * self.color.value:
                 moves.append((-1, 1 * move_up))
+
         if self.x < 7:
-            if board[self.y + 1 * move_up][self.x + 1] != 0 and board[self.y + 1 * move_up][self.x + 1].color == ChessValues.BLACK:
+            if board[self.y + 1 * move_up][self.x + 1].color.value == -1 * self.color.value:
                 moves.append((1, 1 * move_up))
         return moves
 
 
 class King(ChessPiece):
     def __init__(self, color, x, y):
-        ChessPiece.__init__(color, x, y)
+        ChessPiece.__init__(self, color, x, y)
 
 
 class ChessGame:
@@ -93,28 +74,29 @@ def create_box(surface, x, y, width, height, color):
     box = pygame.Rect((x, y, width, height))
 
     pygame.draw.rect(surface, color, box)
-    pygame.display.flip()
 
 
 def draw_chess_board(surface, chess_board):
     sur_width = surface.get_width()
     sur_height = surface.get_height()
 
-    box_size = sur_height / 10
-    width_offset = (sur_width - sur_height) / 2 + box_size
+    min_size = min(sur_height, sur_width)
+    box_size = min_size / 12
+    width_offset = sur_width / 2 - 4 * box_size
+    height_offset = sur_height / 2 - 4 * box_size
 
     for y in range(8):
         for x in range(8):
-            if ChessValues.WHITE in chess_board[y][x]:
-                create_box(surface=surface, x=(width_offset + x * box_size), y=(box_size + box_size * y),
+            if ChessValues.WHITE == chess_board[y][x]:
+                create_box(surface=surface, x=(width_offset + x * box_size), y=(height_offset + box_size * y),
                            width=box_size,
                            height=box_size, color=constants.COLOR_WHITE)
-            elif ChessValues.BLACK in chess_board[y][x]:
-                create_box(surface=surface, x=(width_offset + x * box_size), y=(box_size + box_size * y),
+            elif ChessValues.BLACK == chess_board[y][x]:
+                create_box(surface=surface, x=(width_offset + x * box_size), y=(height_offset + box_size * y),
                            width=box_size,
                            height=box_size, color=constants.COLOR_GREEN)
-            elif ChessValues.MOVE in chess_board[y][x]:
-                create_box(surface=surface, x=(width_offset + x * box_size), y=(box_size + box_size * y),
+            elif ChessValues.MOVE == chess_board[y][x]:
+                create_box(surface=surface, x=(width_offset + x * box_size), y=(height_offset + box_size * y),
                            width=box_size,
                            height=box_size, color=constants.COLOR_YELLOW)
     pygame.display.flip()
