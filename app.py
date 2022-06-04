3446757324b6a3bd32d44cf4ab843f652cbbf312
Vel <ukeruk@gmail.com>
2022-06-04 10:37:45 +0300

    Created all Chess pieces.

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ import constants
 
 
 class ChessValues(Enum):
-    BLACK = -1
+    WHITE = -1
     EMPTY = 0
-    WHITE = 1
+    BLACK = 1
     HIGHLIGHT = 2
     HOVER = 3
     MOVE = 4
@@ -23,7 +23,7 @@ def assign_chess_board_values():
     for i in range(8):
         board.append([])
         for j in range(8):
-            board[i].append(ChessValues.BLACK if (i+j) % 2 else ChessValues.WHITE)
+            board[i].append(ChessValues.BLACK if (i + j) % 2 else ChessValues.WHITE)
     return board
 
 
@@ -32,6 +32,11 @@ class ChessPiece:
         self.color = color
         self.x = x
         self.y = y
+
+
+class EmptyPiece:
+    def __init__(self):
+        self.color = ChessValues.EMPTY
 
 
 class Pawn(ChessPiece):
@@ -63,11 +68,144 @@ class Pawn(ChessPiece):
 class King(ChessPiece):
     def __init__(self, color, x, y):
         ChessPiece.__init__(self, color, x, y)
+        self.moved = False
+
+    def available_moves(self, board):
+        moves = []
+
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if not i == j == 0:
+                    if 7 >= self.x + i >= 0 and 7 >= self.y + j >= 0:
+                        if board[self.y + j][self.x + i].color.value != self.color.value:
+                            moves.append((i, j))
+
+        if not self.moved:
+            if type(board[self.y][0]) == type(Rook) and board[self.y][0].color == self.color \
+                    and board[self.y][0].moved is False:
+                if board[self.y][1] == 0 and board[self.y][2] == 0 and board[self.y][3] == 0:
+                    moves.append((-2, 0))
+
+            if type(board[self.y][7]) == type(Rook) and board[self.y][7].color == self.color \
+                    and board[self.y][7].moved is False:
+                if board[self.y][5] == 0 and board[self.y][6] == 0:
+                    moves.append((2, 0))
+
+
+class Rook(ChessPiece):
+    def __init__(self, color, x, y):
+        ChessPiece.__init__(self, color, x, y)
+        self.moved = False
+
+    def available_moves(self, board):
+        moves = []
+
+        for j in ([0, 1], [1, 0], [-1, 0], [0, -1]):
+            move = True
+            for i in range(1, 8):
+                if move:
+                    if 7 >= self.y + i * j[0] >= 0 and 7 >= self.x + i * j[1] >= 0:
+                        if board[self.y + i * j[0]][self.x + i * j[1]].color is not self.color:
+                            moves.append((i * j[1], i * j[0]))
+                        if board[self.y + i * j[0]][self.x + i * j[1]].color.value != ChessValues.EMPTY:
+                            move = False
+                    else:
+                        move = False
+        return moves
+
+
+class Bishop(ChessPiece):
+    def __init__(self, color, x, y):
+        ChessPiece.__init__(self, color, x, y)
+
+    def available_moves(self, board):
+        moves = []
+
+        for j in ([1, 1], [-1, 1], [-1, -1], [1, -1]):
+            move = True
+            for i in range(1, 8):
+                if move:
+                    if 7 >= self.y + i * j[0] >= 0 and 7 >= self.x + i * j[1] >= 0:
+                        if board[self.y + i * j[0]][self.x + i * j[1]].color is not self.color:
+                            moves.append((i * j[1], i * j[0]))
+                        if board[self.y + i * j[0]][self.x + i * j[1]].color.value != ChessValues.EMPTY:
+                            move = False
+                    else:
+                        move = False
+        return moves
+
+
+class Knight(ChessPiece):
+    def __init__(self, color, x, y):
+        ChessPiece.__init__(self, color, x, y)
+
+    def available_moves(self, board):
+        moves = []
+
+        for j in ([2, 1], [-2, 1], [-2, -1], [2, -1], [1, 2], [-1, 2], [-1, -2], [1, -2]):
+            if 7 >= self.y + j[0] >= 0 and 7 >= self.x + j[1] >= 0:
+                if board[self.y + j[0]][self.x + j[1]].color is not self.color:
+                    moves.append((j[1], j[0]))
+        return moves
+
+
+class Queen(ChessPiece):
+    def __init__(self, color, x, y):
+        ChessPiece.__init__(self, color, x, y)
+
+    def available_moves(self, board):
+        moves = []
+
+        for j in ([1, 1], [-1, 1], [-1, -1], [1, -1], [0, 1], [1, 0], [-1, 0], [0, -1]):
+            move = True
+            for i in range(1, 8):
+                if move:
+                    if 7 >= self.y + i * j[0] >= 0 and 7 >= self.x + i * j[1] >= 0:
+                        if board[self.y + i * j[0]][self.x + i * j[1]].color is not self.color:
+                            moves.append((i * j[1], i * j[0]))
+                        if board[self.y + i * j[0]][self.x + i * j[1]].color.value != ChessValues.EMPTY:
+                            move = False
+                    else:
+                        move = False
 
 
 class ChessGame:
     def __init__(self):
         self.board = []
+        self.create_board()
+
+    def create_board(self):
+        brd = constants.CHESS_BOARD
+        empty = EmptyPiece()
+        for i in range(len(brd)):
+            self.board.append([])
+            for j in range(len(brd[i])):
+                if 'w' in brd[i][j]:
+                    color = ChessValues.WHITE
+                elif 'b' in brd[i][j]:
+                    color = ChessValues.BLACK
+                else:
+                    self.board.append(empty)
+                    continue
+                match brd[i][j][0]:
+                    case 'R':
+                        self.board[i].append(Rook(color, j, i))
+                        break
+                    case 'N':
+                        self.board[i].append(Knight(color, j, i))
+                        break
+                    case 'B':
+                        self.board[i].append(Bishop(color, j, i))
+                        break
+                    case 'Q':
+                        self.board[i].append(Queen(color, j, i))
+                        break
+                    case 'K':
+                        self.board[i].append(King(color, j, i))
+                        break
+                    case 'P':
+                        self.board[i].append(Pawn(color, j, i))
+                        break
 
 
 def create_box(surface, x, y, width, height, color):
@@ -106,7 +244,7 @@ pygame.init()
 
 screen = pygame.display.set_mode([1920, 1080], pygame.RESIZABLE)
 
-screen.fill((75, 75, 75))
+screen.fill(constants.BG_COLOR)
 pygame.display.flip()
 
 chess_board = assign_chess_board_values()
@@ -122,7 +260,7 @@ while running:
             running = False
 
         if event.type == pygame.VIDEORESIZE:
-            screen.fill((75, 75, 75))
+            screen.fill(constants.BG_COLOR)
             draw_chess_board(screen, chess_board)
 
 pygame.quit()
--- constants.py
+++ constants.py
@@ -1,3 +1,19 @@
-COLOR_WHITE = (255, 255, 255)
-COLOR_GREEN = (0, 150, 0)
+COLOR_WHITE = (238, 238, 210)
+COLOR_GREEN = (118, 150, 86)
 COLOR_YELLOW = (255, 255, 0)
+
+BG_COLOR = (50, 50, 54)
+
+# First letter is chess piece, second letter is color
+# P = Pawn | R = Rook | N = Knight | B = Bishop | Q = Queen | K = King
+# b = Black | w = White | NA = empty
+CHESS_BOARD = [
+    ['Rb', 'Nb', 'Bb', 'Qb', 'Kb', 'Bb', 'Nb', 'Rb'],
+    ['Pb', 'Pb', 'Pb', 'Pb', 'Pb', 'Pb', 'Pb', 'Pb'],
+    ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+    ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+    ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+    ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+    ['Pw', 'Pw', 'Pw', 'Pw', 'Pw', 'Pw', 'Pw', 'Pw'],
+    ['Rw', 'Nw', 'Bw', 'Qw', 'Kw', 'Bw', 'Nw', 'Rw'],
+]
